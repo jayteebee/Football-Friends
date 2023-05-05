fb0d310880ba64c3283a66ce1b26f1e69c0f21a0
Tom_Court <tomcourt94@icloud.com>
2023-05-05 15:56:34 +0100

    login inputs and button added

--- front-end/football-friends-front-end/football-friends-front-end/src/login.js
+++ front-end/football-friends-front-end/football-friends-front-end/src/login.js
@@ -1,4 +1,16 @@
 import { useState } from "react";
 export default function Login() {
-  return <h1>Login</h1>;
+  return (
+    <div className="loginPage">
+      <h1>Login</h1>
+      <div className="loginForm">
+        <h2>test</h2>
+        <input className="inputUsername" type="text" />
+      <br></br>
+        <input className="inputPassword" type="text" />
+      <br></br>
+        <button className="searchButtonForFootballFriend">Submit</button>
+      </div>
+    </div>
+  );
 }
