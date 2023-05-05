cca1110a4c44ada8db0ec9526cbd83d237abbe6c
Tom_Court <tomcourt94@icloud.com>
2023-05-05 15:18:22 +0100

    div classes for homepage

--- front-end/football-friends-front-end/football-friends-front-end/src/homepage.js
+++ front-end/football-friends-front-end/football-friends-front-end/src/homepage.js
@@ -1,4 +1,27 @@
 import { useState } from "react";
 export default function Homepage() {
-  return <h1>Homepage</h1>;
+  return (
+    <div className="homepage">
+      <h1 class="logo">
+        👍🏻Football Friends
+        <span style={{ display: "inline-block", transform: "scaleX(-1)" }}>
+          👍🏻
+        </span>
+      </h1>
+      <div className="homepageContent">
+        <div className="newUsers">
+          <h2>New Football Friends</h2>
+        </div>
+        <div className="trendingUsers">
+          <h2>Trending Football Friends</h2>
+        </div>
+        <div className="userLikeThis">
+          <h2>Football Friends who like XXX</h2>
+        </div>
+        <div className="localUsers">
+          <h2>Football Friends in XXX</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
