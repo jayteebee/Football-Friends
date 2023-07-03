fbc8ce72e80b25bb4ce0c3eaaa0e8dd2f024b37d
Jack Coots <jack.coots@newtoneurope.com>
2023-07-03 21:17:24 +0100

    edited

--- front-end/football-friends-front-end/football-friends-front-end/src/index.js
+++ front-end/football-friends-front-end/football-friends-front-end/src/index.js
@@ -9,11 +9,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
